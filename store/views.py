edb202eb7af4720980f32b6b5e34365fc91deb2c
yonggoonoh <nohyg90@gmail.com>
2016-01-06 13:32:56 +0900

    Initialization for store's show view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Store # Import store model
+
+def stores(request, store_id):
+	store = get_object_or_404(Store, pk=store_id)
+
+	return render(
+		request,
+		'show_store.html',
+		{
+			'store': store
+		}
+	)
