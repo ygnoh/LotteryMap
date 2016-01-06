7f65076c4ce45507862ad5b454d5626706b39eb7
yonggoonoh <nohyg90@gmail.com>
2016-01-06 13:54:55 +0900

    Make loadNaverMap function

--- static/js/navermap.js
+++ static/js/navermap.js
@@ -1,1 +1,23 @@
-alert("good"); // for test
+function loadNaverMap(lat, lng){
+	// map
+	var oPoint = new nhn.api.map.LatLng(lat, lng);
+	oMap = new nhn.api.map.Map('naver_map', {
+		point : oPoint,
+		zoom : 10,
+		enableWheelZoom : true,
+		enableDragPan : true,
+		enableDblClickZoom : false,
+		mapMode : 0,
+		minMaxLevel : [ 1, 14 ]
+	});
+
+	// marker
+	var oSize = new nhn.api.map.Size(28, 37);
+	var oOffset = new nhn.api.map.Size(14, 37);
+	// TODO
+	var oIcon = new nhn.api.map.Icon("../static/images/marker.png", oSize, oOffset);
+
+	marker = new nhn.api.map.Marker(oIcon)
+	marker.setPoint(oPoint);
+	oMap.addOverlay(marker);
+}
