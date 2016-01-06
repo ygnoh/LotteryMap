9e8041cb0df2c7531f5ecd643f6666518787c23c
yonggoonoh <nohyg90@gmail.com>
2016-01-06 13:19:30 +0900

    Create Store class in store/models.py

--- store/models.py
+++ store/models.py
@@ -1,5 +1,10 @@
+# coding: utf-8
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+	name = models.CharField(max_length=30)
+	address = models.TextField(max_length=200)
+	created_at = models.DataTimeField(auto_now_add=True)
